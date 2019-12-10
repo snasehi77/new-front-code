fccbd402747dd6c51206db9241b6eef6306b87ee
frank-carpio-at-825621895136 <frank.carpio@socobox.co>
2019-12-10 10:28:52 -0500

    add privacy policy

--- src/Components/PrivacyPolicyComponent/PrivacyPolicy.js
+++ src/Components/PrivacyPolicyComponent/PrivacyPolicy.js
@@ -6,7 +6,7 @@ import {terms} from '../../Utils/PrivacyPolicy';
 
 const PrivacyPolicy = () => {
   return (
-    <>
+    <div className="bg-primary pb-3 content-privacy-policy">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <NavLink to="/" className="navbar-brand" href="#">
           <i>
@@ -15,26 +15,25 @@ const PrivacyPolicy = () => {
           </i>
         </NavLink>
       </nav>
-      <div className="container-fluid content-privacy pt-5 mb-3">
+      <div className="container-fluid content-privacy">
         <div className="row justify-content-center">
           <div className="col-lg-10 col-md-8 col-sm-12">
-            <h2 className="font-weight-light">{terms.Title}</h2>
+            <h4 className="font-weight-bold">{terms.Title}</h4>
             <p className="subtitle font-weight-light">{terms.description}</p>
-            <br/>
-            {terms.points.map(t => {
+            {terms.points.map((t, i) => {
               return (
-                <div className="mb-5">
-                  <h5 className="font-weight-bold">{t.title}</h5>
+                <div key={i} className={t.title ? "mt-2" : ""}>
+                  <b className="font-weight-bold">{t.title}</b>
                   <p style={{fontFamily: 'sans-serif'}} className="subtitle font-weight-light ">{t.description}</p>
                   {t.subPoints && <div className="mt-3">
                     <ul>
-                      {t.subPoints.map(s => {
-                        return <li className="font-weight-light">
+                      {t.subPoints.map((s, i) => {
+                        return <li key={i} className="font-weight-light">
                           {s.item}
                           {s.subItems && <div>
                             <ul>
-                              {s.subItems.map(si => {
-                                return <li className="font-weight-light"> {si.subItem} </li>
+                              {s.subItems.map((si, i) => {
+                                return <li key={i} className="font-weight-light"> {si.subItem} </li>
                               })}
                             </ul>
                           </div>}
@@ -49,7 +48,7 @@ const PrivacyPolicy = () => {
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 };
 
--- src/Utils/PrivacyPolicy.js
+++ src/Utils/PrivacyPolicy.js
@@ -1,117 +1,98 @@
 import React from 'react';
 
+const lorem = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum 
+  has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+  of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also
+  the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s 
+  with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop 
+  publishing software like Aldus PageMaker including versions of Lorem Ipsum.`;
+
+const item = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum 
+  has been the industry's.`;
+
+
 export const terms = {
   Title: 'PRIVACY POLICY',
-  description: `This privacy policy (this "Policy") describes how SBXCloud, Inc. and its affiliates and related
-     companies (collectively, "SBXCloud", "Company", "us", "our", and "we") collect, use, and share personal information
-     of users of www.sbxcloud.com website (the "Site") and any service or functionality (together, or individually, the
-     "Service") belonging to, provided by, created by, or operated by Company.`,
+  description: `${lorem}`,
   points: [
     {
       title: 'Compliance with Law 1581 of 2012, Colombia',
-      description: `SBXCloud complies with the provisions of Statutory Law 1581 of 2012, its regulatory decrees
-       and other concordant norms. Information and the personal data that you provide to Company under the activities
-       carried out on the Service are treated in accordance with the policy of Protection of Personal Data and other
-       security measures, so that third parties are not authorized to access it.`
+      description: `${lorem}`
     },
     {
       title: 'The EU-US Privacy Shield Principles',
-      description: `SBXCloud is committed to supporting and adhering to the principles and requirements of the EU-US 
-       Privacy Shield and is subject to the investigatory and enforcement powers of the Federal Trade Commission (FTC).
-       For further details on the Privacy Shield, please visit: www.privacyshield.gov. 
-       The Privacy Shield Principles describe Company’s accountability for any personal data that Company receives
-       and then transfers to a third party. Should a third-party agent that has been engaged by Company to process personal
-       data on its behalf do so in a manner that is inconsistent with the Privacy Shield Principles, Company may remain 
-       responsible and liable thereunder unless Company proves that it is not responsible for the event that led to the damage,
-       the type of data collected, and the purposes for which the data was collected. All individuals must be informed of any 
-       third parties to whom their personal data will be transferred, their right to access such data, and the means for limiting 
-       the use and disclosure of such data.`
+      description: `${lorem}`
+    },
+    {
+      description: `${lorem}`
     },
     {
       title: 'WHAT WE COLLECT',
-      description: `We get information about you in a range of ways.
-        Information You Give Us. We may collect your‎ name, email address, phone number, username, password and any other information you may
-        provide directly on our Service. Information We Get From Others. We may get information about you from other sources. We may add this 
-        to information we get from this Service. Information Automatically Collected. We automatically log information about you and your computer
-        and mobile device when you use our Service. For example, when visiting our Service, we may log browser type, browser language, the website
-        you visited before browsing to our Service, pages you viewed, how long you spent on a page, access times and information about your use of
-        and actions on our Service. Cookies. We may log information using "Cookies" Cookies are small data files stored on your hard drive by a
-        website. We may use both session Cookies (which expire once you close your web browser) and persistent Cookies (which stay on your
-        computer until you delete them) to provide you with a more personal and interactive experience on our Service. This type of information
-        is collected to make the Service more useful to you and to tailor the experience with us to meet your special interests and needs.`
+      description: `${lorem}`
     },
     {
       title: 'MOBILE DEVICES',
-      description: `When you access the Service by or through a mobile device, we may collect certain information automatically, including without
-       limitation the type of mobile device you use, your mobile operating system, IP address, the type of mobile Internet browser you use, your
-       location information, and other statistics. Please see the section regarding Location Information below regarding the use of your location
-       information and your options.`
+      description: `${lorem}`
     },
     {
       title: 'LOCATION INFORMATION',
-      description: `We may use and store information about your location depending on the permissions you have set on your device. We use this
-       information to provide features of our Service, and to improve and customize our Service. You can enable or disable location services
-       when you use our Service at any time, through your mobile device settings.`
+      description: `${lorem}`
     },
     {
       title: 'USE OF PERSONAL INFORMATION',
-      description: `Company may collect personal data that Users may provide voluntarily to the Company. Company is responsible for safeguarding
-       this information and managing it in accordance with the National Constitution and the law. We may use your personal information to:`,
+      description: `${lorem}`,
       subPoints: [
-        {item: `perate, maintain, and improve the Service, products, and other services.`},
-        {item: `respond to comments and questions and provide customer service.`},
-        {item: `send information including confirmations, technical notices, updates, security alerts, and support and administrative messages.`},
-        {item: `communicate about promotions, upcoming events, educational materials, and news about products and services offered by us and our selected partners.`},
-        {item: `link or combine user information with other personal information.`},
-        {item: `send you calendar reminders, and event reminders.`},
-        {item: `enable you to send information, materials, and documents to your clients and other parties.`},
-        {item: `personalize your experience on our Service by informing you of games and Promotions.`},
-        {item: `provide and deliver products and services that customers request.`},
-        {item: `develop the corporate purpose of Company.`},
-        {item: `conduct surveys related to the services or assets of Company.`},
-        {item: `submit commercial business information of Company.`},
-        {item: `develop programs according to Company policies.`},
+        {item: `${item}`},
+        {item: `${item}`},
+        {item: `${item}`},
       ]
     },
     {
       title: 'SHARING OF PERSONAL INFORMATION',
-      description: `We may share personal information:`,
+      description: `${lorem}`,
       subPoints: [
-        {item: 'with Stripe, Inc., our third-party payment processor, in order to process and execute payment transactions approved and/or initiated by you;\n'},
+        {item: `${item}`},
+        {item: `${item}`},
         {
-          item: 'with third parties subject to your consent. For example, you may choose to allow us to share personal information with third-parties for their own' +
-            ' marketing purposes. Such uses will be subject to the each third party’s respective privacy policies.\n'
-        },
-        {
-          item: 'with third parties when we negotiate or execute a business deal involving the sale or transfer of all or a part of our business or assets. These deals' +
-            ' can include any merger, financing, acquisition, or bankruptcy transaction or proceeding.\n'
-        },
-        {
-          item: 'for legal, protection, and safety purposes, including to:\n', subItems: [
-            {
-              subItem: 'comply with applicable laws and regulations, including without limitation any tax,' +
-                ' labor, social security, and commercial laws and regulations.\n'
-            },
-            {
-              subItem: 'comply with Company’s contractual commitments and obligations.\n'
-            },
-            {
-              subItem: 'respond to lawful requests and legal processes.\n',
-            },
-            {
-              subItem: 'protect the rights and property of Company, our agents, customers, and others.' +
-                ' This includes enforcing our agreements, policies, and terms of use.\n'
-            },
-            {
-              subItem: 'share information in an emergency. This includes protecting the safety of our employees and agents, our customers, or any person.\n'
-            }
+          item: `${item}`,
+          subItems: [
+            {subItem: `${item}`},
+            {subItem: `${item}`},
+            {subItem: `${item}`},
+            {subItem: `${item}`},
           ]
         },
-        {
-          item: 'with our employees and third party service providers to enable them to provide services such as email delivery, text messaging, customer support,' +
-            ' voice messaging, auditing, website hosting, information technology, and other services.\n'
-        }
+        {item: `${item}`},
       ]
+    },
+    {
+      title: 'AUTHORIZATION OF THE HOLDER',
+      description: `${lorem}`
+    },
+    {
+      description: `${lorem}`,
+      subPoints: [
+        {item: `${item}`},
+        {item: `${item}`},
+        {item: `${item}`},
+        {item: `${item}`},
+      ]
+    },
+    {
+      title: 'PAYMENTS',
+      description: `${lorem}`
+    },
+    {
+      title: 'SECURITY',
+      description: `${lorem}`
+    },
+    {
+      title: `INFORMATION CHOICES AND CHANGES`,
+      description: `${lorem}`
+    },
+    {
+      title: 'CONTACT INFORMATION',
+      description: `${lorem}`
     }
   ]
 };
