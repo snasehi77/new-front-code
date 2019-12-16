78aaf0b20011847508b60cf48bd4d553b2b1bb6a
frank-carpio-at-825621895136 <frank.carpio@socobox.co>
2019-12-16 10:34:41 -0500

    restart coming soon

--- src/Components/ComingSoonComponent/ComingSoonComponent.js
+++ src/Components/ComingSoonComponent/ComingSoonComponent.js
@@ -7,14 +7,14 @@ const ComingSoonComponent = () => {
 
   const year = new Date().getFullYear();
   return (
-    <div className="bg-primary vh-100 pl-5 d-flex align-items-center">
+    <div className="bg-primary vh-100 bkg-p-auto d-flex align-items-center">
 
       <h4 className="your-case-logo font-weight-bold cl-white">
         <FontAwesomeIcon icon={faUser} className="mr-1"/>
         YourCase
       </h4>
 
-      <p className="bkg-coming-soon">Coming Soon.</p>
+      <p className="bkg-coming-soon p-0 m-0">Coming Soon.</p>
 
       <p className="bkg-inc">
         <small className="white-opacity">
