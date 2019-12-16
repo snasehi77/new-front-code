a9bd70ed585ba554d12f657ced51a30c42234a8c
frank-carpio-at-825621895136 <frank.carpio@socobox.co>
2019-12-16 10:27:20 -0500

    restart coming soon

--- src/Components/ComingSoonComponent/ComingSoonComponent.js
+++ src/Components/ComingSoonComponent/ComingSoonComponent.js
@@ -1,40 +1,26 @@
 import React from 'react';
+import "./ComingSoonComponent.scss";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faUser} from "@fortawesome/free-solid-svg-icons";
-import imgHome from "../../Assets/Images/woman.png";
-import "./ComingSoonComponent.scss";
 
 const ComingSoonComponent = () => {
+
+  const year = new Date().getFullYear();
   return (
-    <div className="content-front vh-100">
-      <h4 className="your-case-logo font-weight-bold">
+    <div className="bg-primary vh-100 pl-5 d-flex align-items-center">
+
+      <h4 className="your-case-logo font-weight-bold cl-white">
         <FontAwesomeIcon icon={faUser} className="mr-1"/>
         YourCase
       </h4>
 
-      <img className="img-home" src={imgHome} alt=""/>
+      <p className="bkg-coming-soon">Coming Soon.</p>
 
-      <div className="row m-0 vh-100 justify-content-center align-items-center">
-        <div className="col-lg-8">
-            <h1 className="title font-weight-bold ptp-10">Coming Soon...</h1>
-            {/*<p className="subtitle font-weight-bolder">We're coming soon! To tell us your cases. <br/>*/}
-            {/*  We're working hard to give you the best experience.</p>*/}
-
-        </div>
-        {/*<div className="col-lg-12 ptp-10 d-flex justify-content-center">*/}
-
-        {/*  <a className="circle-item pointer-event text-decoration-none">*/}
-        {/*    <i title="Instagram" className="fa fa-instagram">{}</i>*/}
-        {/*  </a>*/}
-        {/*  <a className="circle-item pointer-event text-decoration-none">*/}
-        {/*    <i title="Twitter" className="fa fa-twitter">{}</i>*/}
-        {/*  </a>*/}
-        {/*  <a className="circle-item pointer-event text-decoration-none">*/}
-        {/*    <i title="Linkedin" className="fa fa-linkedin">{}</i>*/}
-        {/*  </a>*/}
-
-        {/*</div>*/}
-      </div>
+      <p className="bkg-inc">
+        <small className="white-opacity">
+          <i className="fa fa-copyright">&nbsp;</i>{year} Your Case Inc.
+        </small>
+      </p>
     </div>
   )
 };
