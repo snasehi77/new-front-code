9d0d7f28b6af0cf6ca7aae96b493b86f6685a9c1
frank-carpio-at-825621895136 <frank.carpio@socobox.co>
2019-12-17 16:54:48 -0500

    pending

--- src/Components/ComingSoonComponent/ComingSoonComponent.js
+++ src/Components/ComingSoonComponent/ComingSoonComponent.js
@@ -2,25 +2,24 @@ import React from 'react';
 import "./ComingSoonComponent.scss";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faUser} from "@fortawesome/free-solid-svg-icons";
+import LogoComponent from "../Shared/LogoComponent";
 
 const ComingSoonComponent = () => {
 
   const year = new Date().getFullYear();
   return (
-    <div className="bg-primary vh-100 bkg-p-auto d-flex align-items-center">
+    <div className="bkg-coming-soon vh-100 bkg-p-auto d-flex align-items-center">
 
-      <h4 className="your-case-logo font-weight-bold cl-white">
-        <FontAwesomeIcon icon={faUser} className="mr-1"/>
-        YourCase
-      </h4>
+      <LogoComponent light className="your-case-logo"/>
 
-      <p className="bkg-coming-soon p-0 m-0">Coming Soon.</p>
+      <p className="p-0 m-0">Coming soon.</p>
 
-      <p className="bkg-inc">
+      <p className="bkg-inc pl-2">
         <small className="white-opacity">
-          <i className="fa fa-copyright">&nbsp;</i>{year} Your Case Inc.
+          <i className="fa fa-copyright">&nbsp;</i>{year} YourCase.com, LLC.
         </small>
       </p>
+
     </div>
   )
 };
--- src/Components/HomeComponent/CasesComponent.js
+++ src/Components/HomeComponent/CasesComponent.js
@@ -38,19 +38,10 @@ const cases = [
 
 const CasesComponent = () => {
 
-  window.addEventListener('scroll', () => {
-    let scroll = document.documentElement.scrollTop;
-    let element = document.getElementById("scrolling");
-    if (element) {
-      scroll > 200 ?
-        element.className += " content-cases-transition w-80" :
-        element.className = "content-cases p-10 mb-3 w-70";
-    }
-  });
 
   return (
     <div className="container-fluid">
-      <div id="scrolling" className="content-cases p-10 mb-3 w-70">
+      <div id="scrolling" className="content-cases mb-3 w-80 content-cases-transition ">
         <div className="text-center mb-3 ">
           <h2 className="font-weight-bolder"> Have a case? We can help. </h2>
           <h6 className="white-opacity">We can find you can attorney who focuses on cases like yours</h6>
