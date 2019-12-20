40e5da19b6bf6cfed13573432a85ccb9a331ab65
frank-carpio-at-825621895136 <frank.carpio@socobox.co>
2019-12-20 15:15:28 -0500

    update responsive a to g in mobile

---
+++ deploy/precache-manifest.3b1d4e99751a979ac62c1bed72715907.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffac8564ca23258abc0556f7ebc4d13d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fae1dd79b8a3bdb1ee5",
+    "url": "/static/css/2.411ec52d.chunk.css"
+  },
+  {
+    "revision": "6f477759519307f23e6f",
+    "url": "/static/css/main.67295e1b.chunk.css"
+  },
+  {
+    "revision": "7fae1dd79b8a3bdb1ee5",
+    "url": "/static/js/2.3f654aab.chunk.js"
+  },
+  {
+    "revision": "6f477759519307f23e6f",
+    "url": "/static/js/main.6647c6e8.chunk.js"
+  },
+  {
+    "revision": "a602d1c9f196cb4b12bf",
+    "url": "/static/js/runtime-main.50d3f9d9.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "39aecec499a12237c50cc60b4c10b315",
+    "url": "/static/media/image.39aecec4.png"
+  },
+  {
+    "revision": "1132c19a0d370d39707eb0fed477cccd",
+    "url": "/static/media/man.1132c19a.png"
+  },
+  {
+    "revision": "72b3515a25ee16f9f61725b0f4a21968",
+    "url": "/static/media/woman.72b3515a.png"
+  },
+  {
+    "revision": "6d8bc809d71e258fec35d5fef928dd4f",
+    "url": "/static/media/yourcase-logo-dark.6d8bc809.png"
+  },
+  {
+    "revision": "0f87592443ff20199dd3fdb42bb4e768",
+    "url": "/static/media/yourcase-logo-light.0f875924.png"
+  }
+]);
--- deploy/service-worker.js
+++ deploy/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.58298486237d25467898a88352c87a04.js"
+  "/precache-manifest.3b1d4e99751a979ac62c1bed72715907.js"
 );
 
 self.addEventListener('message', (event) => {
