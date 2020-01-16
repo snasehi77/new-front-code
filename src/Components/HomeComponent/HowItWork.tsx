08a526555aee6efbcdb0d30d27451837305e07c2
Martin Zuleta <martin_zuleta@allcode.com>
2020-01-16 10:40:28 -0500

    man how it works desktop preview

--- src/Components/HomeComponent/HowItWork.tsx
+++ src/Components/HomeComponent/HowItWork.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import {steps} from "../../Utils/Utilities";
+import imgSteps from '../../Assets/Images/man2.png'
 
-const  HowItWork = () => {
+
+const HowItWork = () => {
     return <div className="container-fluid">
         <div className="row m-0">
             <div className="container mt-5 mt-md-0 w-80 ptp-10 pbp-10 content-steps">
@@ -11,12 +13,58 @@ const  HowItWork = () => {
                     {steps.map(s => {
                         return (
                             <div key={s.num} className="col-lg-4 col-md-6 col-12">
-                                <h1 className={s.num === '1' ? "how-it-works-num" : "how-it-works-num mt-5 mt-md-0"}>{s.num}</h1>
-                                <p className="how-it-works-title-num"> {s.title}</p>
-                                <p className="how-it-works-description-num">{s.p1}<br/>{s.p2 && s.p2}</p>
+                                <h1 className={`how-it-works-num text-center ${s.num === '1' ? "" : "mt-5 mt-md-0"}`}>{s.num}</h1>
+                                <p className="how-it-works-title-num text-center mb-0"> {s.title}</p>
+                                <p className="how-it-works-description-num text-center pt-2">{s.p1}<br/>{s.p2 && s.p2}
+                                </p>
                             </div>
                         )
                     })}
+                </div>
+            </div>
+        </div>
+
+        {/*<div className="container">*/}
+        {/*    <div className="row justify-content-center align-items-center">*/}
+        {/*        <div className="col-10 col-sm-2 p-0 d-sm-none">*/}
+        {/*            <img className="img-fluid" style={{background: '#E5E5E5'}} src={imgSteps} alt=""/>*/}
+        {/*        </div>*/}
+        {/*        <div className="col-12 col-sm-8 col-md-8 blue-temp col-lg-8 container-height-steps">*/}
+        {/*            <div className="row h-100 align-items-center">*/}
+        {/*                <div className="col-sm-4 d-none d-sm-block img-steps-div">*/}
+        {/*                    <img className="img-steps" src={imgSteps} alt=""/>*/}
+        {/*                </div>*/}
+
+        {/*                <div className="col-sm-8 pt-md-4 d-block-flex justify-content-center align-items-center">*/}
+        {/*                    <h1 style={{color: '#F9C6C6'}} className="fa-4x text-center ">❝</h1>*/}
+        {/*                    <p className="text-center case-paragraph">*/}
+        {/*                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum*/}
+        {/*                        has been the*/}
+        {/*                        industry's standard dummy text.*/}
+        {/*                    </p>*/}
+        {/*                    <p className="case-paragraph-footer"><strong>Luis, 46</strong><br/>San Mateo, CA</p>*/}
+        {/*                </div>*/}
+        {/*            </div>*/}
+        {/*        </div>*/}
+        {/*    </div>*/}
+        {/*</div>*/}
+
+
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-4 p-0">
+                    <img className="img-fluid" style={{background: '#C4C4C4'}} src={imgSteps} alt="manCases"/>
+                </div>
+                <div className="col-md-4 p-0 d-flex justify-content-center flex-column" style={{backgroundColor: '#002D44'}}>
+                    <div>
+                        <h1 style={{color: '#F9C6C6'}} className="fa-4x text-center">❝</h1>
+                        <p className="text-center case-paragraph">
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
+                            tincidunt
+                            ut laoreet dolore magna aliquam erat volutpat.
+                        </p>
+                        <p className="text-center case-paragraph-footer"><strong>Luis, 46</strong><br/>San Mateo, CA</p>
+                    </div>
                 </div>
             </div>
         </div>
--- src/Utils/Utilities.ts
+++ src/Utils/Utilities.ts
@@ -6,134 +6,153 @@ import icon_sexual_abuse from "../Assets/icons/sexual_abuse 2.png";
 import icon_elder_abuse from "../Assets/icons/elder 2.png";
 
 export function getMonths() {
-  return [
-    {abbreviation: 'Jan', month: 'January'},
-    {abbreviation: 'Feb', month: 'February'},
-    {abbreviation: 'Mar', month: 'March'},
-    {abbreviation: 'Apr', month: 'April'},
-    {abbreviation: 'May', month: 'May'},
-    {abbreviation: 'Jun', month: 'June'},
-    {abbreviation: 'Jul', month: 'July'},
-    {abbreviation: 'Aug', month: 'August'},
-    {abbreviation: 'Sep', month: 'September'},
-    {abbreviation: 'Oct', month: 'October'},
-    {abbreviation: 'Nov', month: 'November'},
-    {abbreviation: 'Dec', month: 'December'},
-  ]
+    return [
+        {abbreviation: 'Jan', month: 'January'},
+        {abbreviation: 'Feb', month: 'February'},
+        {abbreviation: 'Mar', month: 'March'},
+        {abbreviation: 'Apr', month: 'April'},
+        {abbreviation: 'May', month: 'May'},
+        {abbreviation: 'Jun', month: 'June'},
+        {abbreviation: 'Jul', month: 'July'},
+        {abbreviation: 'Aug', month: 'August'},
+        {abbreviation: 'Sep', month: 'September'},
+        {abbreviation: 'Oct', month: 'October'},
+        {abbreviation: 'Nov', month: 'November'},
+        {abbreviation: 'Dec', month: 'December'},
+    ]
 }
 
 
 export function ToArray(map: any) {
-  let temp:any = [];
-  Object.keys(map).forEach(c => {
-    if (map[c]) {
-      temp.push(map[c]);
-    }
-  });
-  return temp;
+    let temp: any = [];
+    Object.keys(map).forEach(c => {
+        if (map[c]) {
+            temp.push(map[c]);
+        }
+    });
+    return temp;
 }
 
 export function toMap(list: Array<any>, key: string) {
-  return list.reduce((acc, item) => {
-    acc[item[key]] = item;
-    return acc;
-  }, {});
+    return list.reduce((acc, item) => {
+        acc[item[key]] = item;
+        return acc;
+    }, {});
 }
 
 export function formatPhoneNumber(phoneNumberString: string, oldValue: string) {
-  if (
-    oldValue.length > phoneNumberString.length &&
-    phoneNumberString.length <= 4
-  ) {
-    if (phoneNumberString.length === 4 || phoneNumberString.length === 1) {
-      return phoneNumberString.slice(0, phoneNumberString.length - 1);
-    } else {
-      return phoneNumberString;
+    if (
+        oldValue.length > phoneNumberString.length &&
+        phoneNumberString.length <= 4
+    ) {
+        if (phoneNumberString.length === 4 || phoneNumberString.length === 1) {
+            return phoneNumberString.slice(0, phoneNumberString.length - 1);
+        } else {
+            return phoneNumberString;
+        }
     }
-  }
-  let phone = phoneNumberString.replace(/\D/g, "");
-  const match = phone.match(/^(\d{1,3})(\d{0,3})(\d{0,4})$/);
-  if (match) {
-    phone = `(${match[1]})${match[2] ? " " : ""}${match[2]}${
-      match[3] ? "-" : ""
-    }${match[3]}`;
-  }
-  return phone;
+    let phone = phoneNumberString.replace(/\D/g, "");
+    const match = phone.match(/^(\d{1,3})(\d{0,3})(\d{0,4})$/);
+    if (match) {
+        phone = `(${match[1]})${match[2] ? " " : ""}${match[2]}${
+            match[3] ? "-" : ""
+        }${match[3]}`;
+    }
+    return phone;
 }
 
 export function formatSSN(SSN: string) {
-  let phone = SSN.replace(/\D/g, "");
-  const match = phone.match(/^(\d{1,3})(\d{0,2})(\d{0,4})$/);
-  if (match) {
-    phone = `${match[1]}${match[2] ? "-" : ""}${match[2]}${
-      match[3] ? "-" : ""
-    }${match[3]}`;
-  }
-  return phone;
+    let phone = SSN.replace(/\D/g, "");
+    const match = phone.match(/^(\d{1,3})(\d{0,2})(\d{0,4})$/);
+    if (match) {
+        phone = `${match[1]}${match[2] ? "-" : ""}${match[2]}${
+            match[3] ? "-" : ""
+        }${match[3]}`;
+    }
+    return phone;
 }
 
 export const checkMail = (mail: string) => {
-  return mail.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+    return mail.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
 };
 
 
 export const cases = [
-  {
-    id: 1,
-    routeUrl: '/accidents_and_injuries/24',
-    label: 'Accidents & Injuries',
-    icon: icon_accidents_and_injuries
-  },
-  {
-    id: 2,
-    routeUrl: '/health_and_medical_issues/24',
-    label: 'Health & Medical Issues',
-    icon: icon_health_and_medical_issues
-  },
-  {
-    id: 3,
-    routeUrl: '/labor_work_place_issues/24',
-    label: 'Labor & WorkPlace Issues',
-    mobileLabel: 'Labor & WorkPlace',
-    icon: icon_labor_work_place_issues
-  },
-  {
-    id: 4,
-    routeUrl: 'sexual_harassment/24',
-    label: 'Sexual Harassment',
-    icon: icon_sexual_harassment
-  },
-  {
-    id: 5,
-    routeUrl: 'sexual_abuse/24',
-    label: 'Sexual Abuse',
-    icon: icon_sexual_abuse
-  },
-  {
-    id: 6,
-    routeUrl: 'elder_abuse/24',
-    label: 'Elder Abuse',
-    icon: icon_elder_abuse
-  }
+    {
+        id: 1,
+        routeUrl: '/accidents_and_injuries/24',
+        label: 'Accidents & Injuries',
+        icon: icon_accidents_and_injuries
+    },
+    {
+        id: 2,
+        routeUrl: '/health_and_medical_issues/24',
+        label: 'Health & Medical Issues',
+        icon: icon_health_and_medical_issues
+    },
+    {
+        id: 3,
+        routeUrl: '/labor_work_place_issues/24',
+        label: 'Labor & WorkPlace Issues',
+        mobileLabel: 'Labor & WorkPlace',
+        icon: icon_labor_work_place_issues
+    },
+    {
+        id: 4,
+        routeUrl: 'sexual_harassment/24',
+        label: 'Sexual Harassment',
+        icon: icon_sexual_harassment
+    },
+    {
+        id: 5,
+        routeUrl: 'sexual_abuse/24',
+        label: 'Sexual Abuse',
+        icon: icon_sexual_abuse
+    },
+    {
+        id: 6,
+        routeUrl: 'elder_abuse/24',
+        label: 'Elder Abuse',
+        icon: icon_elder_abuse
+    }
 ];
 
 export const steps = [
-  {
-    num: '1',
-    title: 'Tell us what happened.',
-    p1: "Answer a few simple questions",
-    p2: " about your case."
-  },
-  {
-    num: '2',
-    title: 'We make an introduction.',
-    p1: "Based on your answers,",
-    p2: " we find an attorney for you."
-  },
-  {
-    num: '3',
-    title: 'They get in touch.',
-    p1: "Within minutes, you'll be introduced to an attorney interested in representing you."
-    // p2: " attorney interested in representing you."
-  }
+    {
+        num: '1',
+        title: 'Tell us what happened.',
+        p1: "Answer a few simple questions about your case."
+    },
+    {
+        num: '2',
+        title: 'We make an introduction.',
+        p1: "Based on your answers, ",
+        p2: " we find an attorney for you."
+    },
+    {
+        num: '3',
+        title: 'They get in touch.',
+        p1: "Within minutes, you’ll be introduced to an attorney interested in representing you."
+    }
 ];
+
+// export const steps = [
+//   {
+//     num: '1',
+//     title: 'Tell us what happened.',
+//     p1: "Answer a few simple questions",
+//     p2: " about your case."
+//   },
+//   {
+//     num: '2',
+//     title: 'We make an introduction.',
+//     p1: "Based on your answers,",
+//     p2: " we find an attorney for you."
+//   },
+//   {
+//     num: '3',
+//     title: 'They get in touch.',
+//     p1: "Within minutes, you'll be introduced to an attorney interested in representing you."
+//     // p2: " attorney interested in representing you."
+//   }
+// ];
