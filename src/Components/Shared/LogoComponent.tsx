faf3023053b81df29fe635e2f4dfe861181a5703
Carlos De Los Reyes <carlos_reyes@allcode.com>
2020-03-09 15:07:54 -0500

    feat: change the logo to green

--- src/Components/Shared/LogoComponent.tsx
+++ src/Components/Shared/LogoComponent.tsx
@@ -1,27 +1,24 @@
-import React from 'react';
-import logoBlack from '../../Assets/Images/yourcase-logo-dark.png'
-import logoWhite from '../../Assets/Images/yourcase-logo-light.png'
+import React from "react";
+import logoBlack from "../../Assets/Images/yourcase-logo-green.png";
+import logoWhite from "../../Assets/Images/yourcase-logo-light.png";
 
 interface props {
-  className?: string,
-  styles?: any,
+  className?: string;
+  styles?: any;
   light?: boolean;
   onClick?: () => void;
 }
 
-const LogoComponent = ({
-                         className,
-                         styles, light,
-                         onClick
-                       }: props) => {
-
-
-  return <img className={className}
-              style={styles}
-              src={light ? logoWhite : logoBlack}
-              alt=""
-              onClick={onClick}/>
-
+const LogoComponent = ({ className, styles, light, onClick }: props) => {
+  return (
+    <img
+      className={className}
+      style={styles}
+      src={light ? logoWhite : logoBlack}
+      alt=""
+      onClick={onClick}
+    />
+  );
 };
 
 export default LogoComponent;
