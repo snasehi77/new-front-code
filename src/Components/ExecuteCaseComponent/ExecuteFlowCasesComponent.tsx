973de9daf5fbc6196bee6892ef0dbf4cc1958a17
frank allcode <frank.carpio@allcode.com>
2020-03-19 15:41:57 -0500

    fix execute cases component

--- src/Components/ExecuteCaseComponent/ExecuteFlowCasesComponent.tsx
+++ src/Components/ExecuteCaseComponent/ExecuteFlowCasesComponent.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import {useParams} from 'react-router-dom';
-import {ExecutionFlow} from "execution-flow";
-import {baseUrl} from "../../Utils/Config";
 import "./ExecuteCaseComponent.scss";
 import {NavbarBrand} from "reactstrap";
 import logo from "../../Assets/Images/yourcase-logo-green.png";
+import {ExecutionFlow} from "execution-flow";
 
 const ExecuteFlowCasesComponent = () => {
     const {id} = useParams();
 
 
     return (
-      <>
-        <NavbarBrand href="/" className="mr-auto">
+      <div className="vh-100">
+        <NavbarBrand style={{height: "5%"}} href="/" className="mr-auto">
           {/*<LogoComponent className="your-case-logo my-auto ml-4 pt-md-3 ml-md-4"/>*/}
-          <img  className="your-case-logo my-auto ml-2 pt-md-3 ml-md-2" src={logo} alt="logo"/>
+          <img className="your-case-logo my-auto ml-2 pt-md-3 ml-md-2" src={logo} alt="logo"/>
         </NavbarBrand>
-        {id && <ExecutionFlow flowId={parseInt(id)}
-                              className="col-xl-4 col-lg-6 col-md-8 col-12 m-auto"/>}
-      </>
+        <div style={{height: "95%"}}>
+          {id && <ExecutionFlow flowId={parseInt(id)}
+                                className="col-xl-4 col-lg-6 col-md-8 col-12 m-auto"/>}
+        </div>
+      </div>
     )
   }
 ;
--- src/Utils/Config.ts
+++ src/Utils/Config.ts
@@ -3,6 +3,6 @@ declare var process: {
     [key: string]: string;
   };
 };
-export const baseUrl =
-  process.env.REACT_APP_BASE_URL;
+
+export const baseUrl = process.env.REACT_APP_BASE_URL;
 export const flow_id = process.env.REACT_APP_FLOW_ID;
