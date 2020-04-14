28877b627c41e54140d576ff68caccc64a450350
Matthew Gipp <m.r.gipp@gmail.com>
2020-04-13 20:06:09 -0400

    progress on execution flow styles

--- src/Components/ExecuteCaseComponent/ExecuteFlowCasesComponent.tsx
+++ src/Components/ExecuteCaseComponent/ExecuteFlowCasesComponent.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import {useParams} from 'react-router-dom';
 import "./ExecuteCaseComponent.scss";
-import {NavbarBrand} from "reactstrap";
-import logo from "../../Assets/Images/yourcase-logo-green.png";
+import NavbarComponent from "../Shared/NavbarComponent";
 import {ExecutionFlow} from "execution-flow";
 import Flow from "../../models/Flow";
 declare var window:any;
@@ -15,11 +14,8 @@ const ExecuteFlowCasesComponent = () => {
 
     return (
       <div className="vh-100">
-        <NavbarBrand style={{height: "5%"}} href="/" className="mr-auto">
-          {/*<LogoComponent className="your-case-logo my-auto ml-4 pt-md-3 ml-md-4"/>*/}
-          <img className="your-case-logo my-auto ml-2 pt-md-3 ml-md-2" src={logo} alt="logo"/>
-        </NavbarBrand>
-        <div style={{height: "94%"}}>
+        <NavbarComponent/>
+        <div className="hm-view">
           {id && <ExecutionFlow onSubmitForm={onSubmitForm} flowId={parseInt(id)}
                                 className="col-xl-4 col-lg-8 col-md-10 col-12 m-auto"/>}
         </div>
