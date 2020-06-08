2fe8883d69748061fe19841e021af38190b54d8b
SunnyQin <sunny@interactivelabs.co>
2020-06-08 16:11:33 +0800

    add side bar for screen (>1024px)

--- src/Components/ExecuteCaseComponent/ExecuteFlowCasesComponent.tsx
+++ src/Components/ExecuteCaseComponent/ExecuteFlowCasesComponent.tsx
@@ -5,6 +5,37 @@ import NavbarComponent from "../Shared/NavbarComponent";
 import {ExecutionFlow} from "../../ExecutionFlow";
 import Flow from "../../models/Flow";
 
+const Steps = [ //TODO:
+  {
+    title: "Step 1",
+    progress: 10,
+  },
+  {
+    title: "Step 2",
+    progress: 20,
+  },
+  {
+    title: "Step 3",
+    progress: 30,
+  },
+  {
+    title: "Step 4",
+    progress: 40,
+  },
+  {
+    title: "Step 5",
+    progress: 50,
+  },
+  {
+    title: "Step 6",
+    progress: 60,
+  },
+  {
+    title: "Step 6",
+    progress: 70,
+  }
+]
+
 declare var window: any;
 const ExecuteFlowCasesComponent = () => {
     const [progress, setProgress] = useState<number>(0);
@@ -20,12 +51,33 @@ const ExecuteFlowCasesComponent = () => {
 
     return (
       <div className="vh-100">
-        <NavbarComponent showProgress progress={progress} />
-        <div id="yourcase-hm-view">
-          {id && <ExecutionFlow onSubmitForm={onSubmitForm}
-                                flowId={parseInt(id)}
-                                onChangeStep={(data) => setProgress(Math.min(data.length * 10, 100))}
-                                className="yourcase-hm-view-inner-wrapper m-auto"/>}
+        <NavbarComponent 
+          showProgress 
+          progress={progress}
+          light={false}
+        />
+        <div className="hm-d-flex">
+          <div className="hm-sidebar">
+            {Steps.map((item) => {
+              return (
+                <div className="hm-d-flex hm-just-center hm-align-items-center">
+                  <div className={"hm-bar" + (item.progress === progress ? " hm-bar-selected" : "")} />
+                  <div className={"hm-bar-text" + (item.progress === progress ? " hm-bar-text-selected" : "")}>
+                    {item.title}
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+          <div className="hm-flow">
+            <div id="yourcase-hm-view">
+              {id && <ExecutionFlow onSubmitForm={onSubmitForm}
+                                    flowId={parseInt(id)}
+                                    onChangeStep={(data) => setProgress(Math.min(data.length * 10, 100))}
+                                    className="yourcase-hm-view-inner-wrapper m-auto"/>}
+            </div>
+          </div>
+
         </div>
       </div>
     )
--- src/Components/Shared/NavbarComponent.tsx
+++ src/Components/Shared/NavbarComponent.tsx
@@ -1,38 +1,58 @@
 import React, {useState} from 'react';
 import {Collapse, Nav, Navbar, NavbarBrand, NavbarToggler} from "reactstrap";
-import logo from '../../Assets/Images/yourcase-logo-green.png'
+import whiteLogo from '../../Assets/Images/yourcase-logo-light.png'
+import darkLogo from '../../Assets/Images/yourcase-logo-green.png'
+import reactResponsive from 'react-responsive'
 
+const MediaQuery = reactResponsive
+
+const Media = {
+    small: {
+        minWidth: 0,
+        maxWidth: "1024px"
+    },
+    large: {
+        minWidth: "1025px",
+        maxWidth: "100000px"
+    }
+}
 
 const NavbarComponent = (props: any) => {
-    const { showProgress, progress } = props
+    const { showProgress, progress, light = true } = props
     const [collapsed, setCollapsed] = useState<boolean>(true);
     const toggleNavbar = () => setCollapsed(!collapsed);
 
-    return <div className="container-fluid position-absolute p-0">
-        {!!showProgress && <div className="progress step-bar">
-          <div 
-            id="progress" 
-            className={"progress-bar step-track-color " + ("step-track-width" + "-" + progress.toString())}>
-          </div>
-        </div>}
-        <Navbar className="navbar" light>
-            <NavbarBrand href="/" className="mr-auto">
-                {/*<LogoComponent className="your-case-logo my-auto ml-4 pt-md-3 ml-md-4"/>*/}
-                <img  className="your-case-logo my-auto" src={logo} alt="logo"/>
-            </NavbarBrand>
-            <NavbarToggler onClick={toggleNavbar} className="mr-2 d-block d-lg-none" style={{border: '0'}}/>
-            <Collapse isOpen={!collapsed} navbar>
-                <Nav navbar>
-                    {/*<NavItem>*/}
-                    {/*    <NavLink href="/components/">Components</NavLink>*/}
-                    {/*</NavItem>*/}
-                    {/*<NavItem>*/}
-                    {/*    <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>*/}
-                    {/*</NavItem>*/}
-                </Nav>
-            </Collapse>
-        </Navbar>
-    </div>
+    return (
+        <div className="container-fluid position-absolute p-0">
+            {!!showProgress && <div className="progress step-bar">
+            <div 
+                id="progress" 
+                className={"progress-bar step-track-color " + ("step-track-width" + "-" + progress.toString())}>
+            </div>
+            </div>}
+            <Navbar className="navbar" light>
+                <NavbarBrand href="/" className="mr-auto">
+                    <MediaQuery {...Media.small}>
+                        <img  className="your-case-logo my-auto" src={darkLogo} alt="logo"/>
+                    </MediaQuery>
+                    <MediaQuery {...Media.large}>
+                        <img  className="your-case-logo my-auto" src={light ? darkLogo : whiteLogo} alt="logo"/>
+                    </MediaQuery>
+                </NavbarBrand>
+                <NavbarToggler onClick={toggleNavbar} className="mr-2 d-block d-lg-none" style={{border: '0'}}/>
+                <Collapse isOpen={!collapsed} navbar>
+                    <Nav navbar>
+                        {/*<NavItem>*/}
+                        {/*    <NavLink href="/components/">Components</NavLink>*/}
+                        {/*</NavItem>*/}
+                        {/*<NavItem>*/}
+                        {/*    <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>*/}
+                        {/*</NavItem>*/}
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </div>
+    )
 };
 
 export default NavbarComponent;
