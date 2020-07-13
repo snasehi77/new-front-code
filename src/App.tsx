d1518e744aeb2938df21af1b04a0eb7c15d7cb10
Ben W <5994802+benwong0@users.noreply.github.com>
2020-07-12 21:32:04 -0700

    Global pw protection for dev (#5)

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,74 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import Routes from './Routes'
-import {HashRouter as Router, Route, Switch} from "react-router-dom";
-import {http, login} from "./Network";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { http, login } from "./Network";
 //import ComingSoonComponent from "./components/ComingSoonComponent/ComingSoonComponent";
-import {initialize} from "./ExecutionFlow";
+import { initialize } from "./ExecutionFlow";
 
 
 const App = () => {
 
+  const simplePw = '2020Tugboat!';
+  const [authenticated, setAuthenticated] = useState(false);
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const s3Bucket = process.env['REACT_APP_S3_BUCKET'];
+  const isDev = s3Bucket && s3Bucket.startsWith('s3://dev.')
+
+  const onPwSubmit = ((e: any) => {
+    e.preventDefault();
+
+    if (password === simplePw) {
+      setAuthenticated(true);
+    } else {
+      setError('Invalid password');
+    }
+  })
+
+  const pwForm = (() => {
+    return (
+      <div className='global-login'>
+        <form onSubmit={onPwSubmit}>
+          {error ? <div className='error-msg'>{error}</div> : null}
+
+          <div>
+            <input type="password" value={password} onChange={(e: any) => {
+              setError('');
+              setPassword(e.currentTarget.value);
+            }} />
+          </div>
+          <div>
+            <input type="submit" value="Login" />
+          </div>
+        </form>
+      </div>
+    );
+  })
+
+  const router = (() => {
+    return (
+      // <ComingSoonComponent/>
+      <Router>
+        <Switch>
+          {Routes.map(r => (
+            <Route exact key={r.id} path={r.path}>
+              <r.component />
+            </Route>)
+          )}
+        </Switch>
+      </Router>
+    );
+  })
+
+
   useEffect(() => {
-    login({username: 'hans@allcode.com', password: 'wpwd'});
+    login({ username: 'hans@allcode.com', password: 'wpwd' });
     initialize(http);
   }, []);
 
-  return (
-    // <ComingSoonComponent/>
-    <Router>
-      <Switch>
-        {Routes.map(r => (
-          <Route exact key={r.id} path={r.path}>
-            <r.component/>
-          </Route>)
-        )}
-      </Switch>
-    </Router>
-  )
+  const ui = !isDev || authenticated ? router() : pwForm();
+  return ui;
 };
 
 export default App;
